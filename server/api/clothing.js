998941d27ff713b35f4297c1a74a83b1beba1c99
Lucy Blagg <lvblagg@gmail.com>
2022-01-11 18:02:30 -0500

    deleted unnecessary routes

--- server/api/clothing.js
+++ server/api/clothing.js
@@ -1,52 +1,45 @@
-const router = require('express').Router()
-const { models: { Clothing }} = require('../db')
-module.exports = router
+const router = require("express").Router();
+const {
+  models: { Clothing },
+} = require("../db");
+module.exports = router;
 
-router.get('/', async (req, res, next) => {
-    try {
-      const clothing = await Clothing.findAll()
-      res.json(clothing)
-    } catch (err) {
-      next(err)
-    }
-})
+router.get("/", async (req, res, next) => {
+  try {
+    const clothing = await Clothing.findAll();
+    res.json(clothing);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.get('/:clothingName', async (req, res, next) => {
-    try {
-      const clothing = await Clothing.findAll({
-          where: {
-              name: req.params.clothingName
-          }
-      })
-      res.json(clothing)
-    } catch (err) {
-      next(err)
-    }
-})
+router.get("/:clothingType", async (req, res, next) => {
+  try {
+    const clothing = await Clothing.findAll({
+      where: {
+        type: req.params.clothingType,
+      },
+    });
+    res.json(clothing);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.get('/:clothingName/:clothingId', async (req, res, next) => {
-    try {
-      const clothing = await Clothing.findAll({
-          where: {
-              name: req.params.clothingName
-          }
-      })
-      res.json(clothing)
-    } catch (err) {
-      next(err)
-    }
-})
-  
-router.put('/:clothingName/:clothingId', async (req, res, next) => {
-    try {
-      const item = await Clothing.findByPk(req.params.clothingId)
-      res.json(item)
-    } catch (err) {
-      next(err)
-    }
-})
+router.get("/:clothingId", async (req, res, next) => {
+  try {
+    const item = await Clothing.findByPk(req.params.clothingId);
+    res.json(item);
+  } catch (err) {
+    next(err);
+  }
+});
 
-
-
-
-
+router.put("/:clothingId", async (req, res, next) => {
+  try {
+    const item = await Clothing.findByPk(req.params.clothingId);
+    res.json(item);
+  } catch (err) {
+    next(err);
+  }
+});
