a298a92666039699fa48e8f37e0afb2abf4ce8d5
Tenzin Choetso <tenzinchoetso93@gmail.com>
2022-01-11 18:01:21 -0500

    user router post injection attack protection

--- server/api/users.js
+++ server/api/users.js
@@ -1,17 +1,41 @@
-const router = require('express').Router()
-const { models: { User }} = require('../db')
-module.exports = router
+const router = require("express").Router();
+const { user } = require("pg/lib/defaults");
+const {
+  models: { User },
+} = require("../db");
+module.exports = router;
 
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
     const users = await User.findAll({
       // explicitly select only the id and username fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
-      attributes: ['id', 'username']
-    })
-    res.json(users)
+      attributes: ["id", "username"],
+    });
+    res.json(users);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
+
+router.post("/signup", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.create({ username, password });
+    res.send({ token: await user.generateToken() });
+  } catch (err) {
+    if (err.name === "SequelizeUniqueConstraintError") {
+      res.status(401).send("User already exists");
+    } else {
+      next(err);
+    }
+  }
+});
+
+// router.get("/cart", async (req, res, next) => {
+//   try {
+//   } catch (err) {
+//     next(err);
+//   }
+// });
