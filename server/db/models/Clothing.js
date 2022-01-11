d208694cd0d065c3de3a9c4cf578d4916e1839e0
Lucy Blagg <lvblagg@gmail.com>
2022-01-11 15:40:06 -0500

    added clothing and user  models to database

---
+++ server/db/models/Clothing.js
@@ -0,0 +1,34 @@
+const Sequelize = require("sequelize");
+const db = require("../db");
+
+const Clothing = db.define("clothing", {
+  type: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: Sequelize.DECIMAL(10, 2),
+    allowNull: false,
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+  color: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  size: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  rating: {
+    type: Sequelize.DECIMAL(3, 2),
+  },
+});
+
+module.exports = Clothing;
