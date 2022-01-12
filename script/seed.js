f4c8f7ad4995dfd3ea34db0b1cae866df5c9efeb
Tenzin Choetso <tenzinchoetso93@gmail.com>
2022-01-12 15:11:30 -0500

    shoes seeded successfully

--- script/seed.js
+++ script/seed.js
@@ -1,284 +1,262 @@
 "use strict";
 
-const db = require("../server");
-const User = require("../server/db/models/User");
-const Clothing = require("../server/db/models/Clothing");
-
+const {
+  db,
+  models: { User, Clothing },
+} = require("../server/db");
 /**
  * seed - this function clears the database, updates tables to
  *      match the models, and populates the database.
  */
-// async function seed() {
-//   await db.sync({ force: true }); // clears db and matches models to tables
-//   console.log("db synced!");
+async function seed() {
+  await db.sync({ force: true }); // clears db and matches models to tables
+  console.log("db synced!");
 
-const shoes = [
-  {
-    type: "shoes",
-    name: "Converse X COMME des GARÇONS PLAY",
-    price: 150.0,
-    description: "Unisex high top shoe",
-    quantity: 80,
-    color: "Grey",
-    rating: 4.66,
-    imageUrl:
-      "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_1086,w_724/c_scale,h_480/f_auto,q_auto/221246M236000_1/comme-des-garcons-play-grey-converse-edition-half-heart-chuck-70-sneakers.jpg",
-  },
-  {
-    type: "shoes",
-    name: "Converse X COMME des GARÇONS PLAY",
-    price: 150.0,
-    description: "Unisex low top shoe",
-    quantity: 68,
-    color: "Navy",
-    rating: 3.47,
-    imageUrl:
-      "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_1086,w_724/c_scale,h_480/f_auto,q_auto/221246M237001_1/comme-des-garcons-play-blue-converse-edition-half-heart-chuck-70-sneakers.jpg",
-  },
-  {
-    type: "shoes",
-    name: "Converse X COMME des GARÇONS PLAY",
-    price: 150.0,
-    description: "Unisex high top shoe",
-    quantity: 75,
-    color: "Navy",
-    rating: 4.32,
-    imageURL:
-      "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_1086,w_724/c_scale,h_480/f_auto,q_auto/221246M236001_1/comme-des-garcons-play-blue-converse-edition-half-heart-chuck-70-sneakers.jpg",
-  },
-  {
-    type: "shoes",
-    name: "Converse X COMME des GARÇONS PLAY",
-    price: 150.0,
-    description: "Unisex low top shoe",
-    quantity: 23,
-    color: "Grey",
-    rating: 4.89,
-    imageURL:
-      "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_1086,w_724/c_scale,h_480/f_auto,q_auto/221246M236000_1/comme-des-garcons-play-grey-converse-edition-half-heart-chuck-70-sneakers.jpg",
-  },
-  {
-    type: "shoes",
-    name: "Black Chuck Taylor All Star Platform Hi Sneakers",
-    price: 65.0,
-    description: "Unisex high top shoe",
-    quantity: 100,
-    color: "Black",
-    rating: 4.98,
-    imageURL:
-      "https://img.ssensemedia.com/images/b_white,g_center,f_auto,q_auto:best/221799M236094_1/converse-black-chuck-taylor-all-star-platform-hi-sneakers.jpg",
-  },
-  {
-    type: "shoes",
-    name: "Beige Summer Daze Chuck 70 sneakers",
-    price: 45.0,
-    description: "Unisex low top shoe",
-    quantity: 89,
-    color: "Beige",
-    rating: 2.34,
-    imageURL:
-      "https://img.ssensemedia.com/images/b_white,g_center,f_auto,q_auto:best/211799M237109_1/converse-beige-summer-daze-chuck-70-sneakers.jpg",
-  },
-];
+  const shoes = await Promise.all([
+    Clothing.create({
+      type: "shoes",
+      name: "Converse X COMME des GARÇONS PLAY",
+      price: 150.0,
+      description: "Unisex high top shoe",
+      quantity: 80,
+      color: "Grey",
+      rating: 4.66,
+      imageUrl:
+        "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_1086,w_724/c_scale,h_480/f_auto,q_auto/221246M236000_1/comme-des-garcons-play-grey-converse-edition-half-heart-chuck-70-sneakers.jpg",
+    }),
+    Clothing.create({
+      type: "shoes",
+      name: "Converse X COMME des GARÇONS PLAY",
+      price: 150.0,
+      description: "Unisex low top shoe",
+      quantity: 68,
+      color: "Navy",
+      rating: 3.47,
+      imageUrl:
+        "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_1086,w_724/c_scale,h_480/f_auto,q_auto/221246M237001_1/comme-des-garcons-play-blue-converse-edition-half-heart-chuck-70-sneakers.jpg",
+    }),
+    Clothing.create({
+      type: "shoes",
+      name: "Converse X COMME des GARÇONS PLAY",
+      price: 150.0,
+      description: "Unisex high top shoe",
+      quantity: 75,
+      color: "Navy",
+      rating: 4.32,
+      imageURL:
+        "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_1086,w_724/c_scale,h_480/f_auto,q_auto/221246M236001_1/comme-des-garcons-play-blue-converse-edition-half-heart-chuck-70-sneakers.jpg",
+    }),
+    Clothing.create({
+      type: "shoes",
+      name: "Converse X COMME des GARÇONS PLAY",
+      price: 150.0,
+      description: "Unisex low top shoe",
+      quantity: 23,
+      color: "Grey",
+      rating: 4.89,
+      imageURL:
+        "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_1086,w_724/c_scale,h_480/f_auto,q_auto/221246M236000_1/comme-des-garcons-play-grey-converse-edition-half-heart-chuck-70-sneakers.jpg",
+    }),
+  ]);
+}
+// const accessories = [
+//   {
+//     type: "accessories",
+//     name: "1.5mm Black Diamond Prong WG TL BVLA",
+//     price: 140.0,
+//     description: "earring",
+//     color: "gold",
+//     quantity: 5,
+//     imageUrl:
+//       "https://www.ninemoonspiercings.com/uploads/1/3/1/5/131540467/s939830397503292869_p463_i3_w2700.jpeg?width=2560?width=800",
+//   },
+//   {
+//     type: "accessories",
+//     name: "Chubby Huggy Hoop, Silver (Single)",
+//     price: 38.0,
+//     description: "earring",
+//     color: "silver",
+//     quantity: 24,
+//     imageUrl:
+//       "https://www.catbirdnyc.com/media/catalog/product/cache/76a94e0cda397936c0138d2cf05d7fe1/h/u/huggiehoop-ss-p1.jpg",
+//   },
+//   {
+//     type: "accessories",
+//     name: "Sweet Nothing Bracelet",
+//     price: 98.0,
+//     description: "bracelet",
+//     color: "gold",
+//     quantity: 40,
+//     imageUrl:
+//       "https://www.catbirdnyc.com/media/catalog/product/cache/76a94e0cda397936c0138d2cf05d7fe1/c/a/catbird-sweetnothingbracelet-yg-p1.jpg",
+//   },
+//   {
+//     type: "accessories",
+//     name: "Mini Cloud Bag",
+//     price: 46.0,
+//     description: "bag",
+//     color: "apricot",
+//     quantity: 39,
+//     imageUrl:
+//       "https://cdn.shopify.com/s/files/1/0851/3262/products/ea0316b8321c0af3b622b767c4c316141582bc2e-2048x2560_1200x1500.jpg?v=1629414849",
+//   },
+//   {
+//     type: "accessories",
+//     name: "Pink & Gold Barocco Print Scarf",
+//     price: 129.0,
+//     description: "scarf",
+//     color: "pink",
+//     quantity: 12,
+//     imageUrl:
+//       "https://img.ssensemedia.com/images/f_auto,q_auto:best/212404F029000_1/versace-pink-and-gold-barocco-print-scarf.jpg",
+//   },
+// ];
 
-const accessories = [
-  {
-    type: "accessories",
-    name: "1.5mm Black Diamond Prong WG TL BVLA",
-    price: 140.0,
-    description: "earring",
-    color: "gold",
-    quantity: 5,
-    imageUrl:
-      "https://www.ninemoonspiercings.com/uploads/1/3/1/5/131540467/s939830397503292869_p463_i3_w2700.jpeg?width=2560?width=800",
-  },
-  {
-    type: "accessories",
-    name: "Chubby Huggy Hoop, Silver (Single)",
-    price: 38.0,
-    description: "earring",
-    color: "silver",
-    quantity: 24,
-    imageUrl:
-      "https://www.catbirdnyc.com/media/catalog/product/cache/76a94e0cda397936c0138d2cf05d7fe1/h/u/huggiehoop-ss-p1.jpg",
-  },
-  {
-    type: "accessories",
-    name: "Sweet Nothing Bracelet",
-    price: 98.0,
-    description: "bracelet",
-    color: "gold",
-    quantity: 40,
-    imageUrl:
-      "https://www.catbirdnyc.com/media/catalog/product/cache/76a94e0cda397936c0138d2cf05d7fe1/c/a/catbird-sweetnothingbracelet-yg-p1.jpg",
-  },
-  {
-    type: "accessories",
-    name: "Mini Cloud Bag",
-    price: 46.0,
-    description: "bag",
-    color: "apricot",
-    quantity: 39,
-    imageUrl:
-      "https://cdn.shopify.com/s/files/1/0851/3262/products/ea0316b8321c0af3b622b767c4c316141582bc2e-2048x2560_1200x1500.jpg?v=1629414849",
-  },
-  {
-    type: "accessories",
-    name: "Pink & Gold Barocco Print Scarf",
-    price: 129.0,
-    description: "scarf",
-    color: "pink",
-    quantity: 12,
-    imageUrl:
-      "https://img.ssensemedia.com/images/f_auto,q_auto:best/212404F029000_1/versace-pink-and-gold-barocco-print-scarf.jpg",
-  },
-];
+// const tops = [
+//   {
+//     type: "tops",
+//     name: "something top",
+//     price: 150.0,
+//     description: "shirt",
+//     color: "green",
+//     quantity: 89,
+//     ImageURL:
+//       "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_1086,w_724/c_scale,h_480/f_auto,q_auto/221232F107011_1/rick-owens-silver-seb-blouse.jpg",
+//   },
+//   {
+//     type: "tops",
+//     name: "something top",
+//     price: 140.0,
+//     description: "sweatshirt",
+//     color: "red",
+//     quantity: 56,
+//     ImageURL:
+//       "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_1086,w_724/c_scale,h_480/f_auto,q_auto/221232F107011_1/rick-owens-silver-seb-blouse.jpg",
+//   },
+//   {
+//     type: "tops",
+//     name: "something top",
+//     price: 130.0,
+//     description: "tank",
+//     color: "blue",
+//     quantity: 90,
+//     imageURL:
+//       "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_1086,w_724/c_scale,h_480/f_auto,q_auto/221232F107011_1/rick-owens-silver-seb-blouse.jpg",
+//   },
+//   {
+//     type: "tops",
+//     name: "something top",
+//     price: 130.0,
+//     description: "sweater",
+//     color: "gold",
+//     quantity: 78,
+//     imageURL:
+//       "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_1086,w_724/c_scale,h_480/f_auto,q_auto/221232F107011_1/rick-owens-silver-seb-blouse.jpg",
+//   },
+//   {
+//     type: "tops",
+//     name: "something top",
+//     price: 250.0,
+//     description: "jacket",
+//     color: "white",
+//     quantity: 67,
+//     imageURL:
+//       "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_1086,w_724/c_scale,h_480/f_auto,q_auto/221232F107011_1/rick-owens-silver-seb-blouse.jpg",
+//   },
+// ];
 
-const tops = [
-  {
-    type: "tops",
-    name: "something top",
-    price: 150.0,
-    description: "shirt",
-    color: "green",
-    quantity: 89,
-    ImageURL:
-      "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_1086,w_724/c_scale,h_480/f_auto,q_auto/221232F107011_1/rick-owens-silver-seb-blouse.jpg",
-  },
-  {
-    type: "tops",
-    name: "something top",
-    price: 140.0,
-    description: "sweatshirt",
-    color: "red",
-    quantity: 56,
-    ImageURL:
-      "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_1086,w_724/c_scale,h_480/f_auto,q_auto/221232F107011_1/rick-owens-silver-seb-blouse.jpg",
-  },
-  {
-    type: "tops",
-    name: "something top",
-    price: 130.0,
-    description: "tank",
-    color: "blue",
-    quantity: 90,
-    imageURL:
-      "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_1086,w_724/c_scale,h_480/f_auto,q_auto/221232F107011_1/rick-owens-silver-seb-blouse.jpg",
-  },
-  {
-    type: "tops",
-    name: "something top",
-    price: 130.0,
-    description: "sweater",
-    color: "gold",
-    quantity: 78,
-    imageURL:
-      "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_1086,w_724/c_scale,h_480/f_auto,q_auto/221232F107011_1/rick-owens-silver-seb-blouse.jpg",
-  },
-  {
-    type: "tops",
-    name: "something top",
-    price: 250.0,
-    description: "jacket",
-    color: "white",
-    quantity: 67,
-    imageURL:
-      "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_1086,w_724/c_scale,h_480/f_auto,q_auto/221232F107011_1/rick-owens-silver-seb-blouse.jpg",
-  },
-];
+// const pants = [
+//   {
+//     type: "pants",
+//     name: "something pants",
+//     price: 120.0,
+//     description: "flowy skirt",
+//     color: "yellow",
+//     quantity: 999,
+//     ImageURL:
+//       "https://img.ssensemedia.com/images/f_auto,q_auto:best/212771F092000_1/toteme-yellow-raw-trim-skirt.jpg",
+//   },
+//   {
+//     type: "pants",
+//     name: "something pants",
+//     price: 140.0,
+//     description: "pencil skirt",
+//     color: "orange",
+//     quantity: 999,
+//     ImageURL:
+//       "https://img.ssensemedia.com/images/f_auto,q_auto:best/202387F090217_1/kenzo-orange-straight-miniskirt.jpg",
+//   },
+//   {
+//     type: "pants",
+//     name: "something pants",
+//     price: 130.0,
+//     description: "tapered pants",
+//     color: "white",
+//     quantity: 999,
+//     imageURL:
+//       "https://img.ssensemedia.com/images/f_auto,q_auto:best/212771F087003_1/toteme-white-city-sport-trousers.jpg",
+//   },
+//   {
+//     type: "pants",
+//     name: "something pants",
+//     price: 140.0,
+//     description: "baggy pants",
+//     color: "black",
+//     quantity: 999,
+//     imageURL:
+//       "https://img.ssensemedia.com/images/b_white,g_center,f_auto,q_auto:best/212704M191021_1/wooyoungmi-black-baggy-carpenter-trousers.jpg",
+//   },
+//   {
+//     type: "pants",
+//     name: "something pants",
+//     price: 85.0,
+//     description: "long short",
+//     color: "pink",
+//     quantity: 999,
+//     imageURL:
+//       "https://img.ssensemedia.com/images/b_white,g_center,f_auto,q_auto:best/211236F088057_1/collina-strada-ssense-exclusive-pink-flower-patch-shorts.jpg",
+//   },
+// ];
 
-const pants = [
-  {
-    type: "pants",
-    name: "something pants",
-    price: 120.0,
-    description: "flowy skirt",
-    color: "yellow",
-    quantity: 999,
-    ImageURL:
-      "https://img.ssensemedia.com/images/f_auto,q_auto:best/212771F092000_1/toteme-yellow-raw-trim-skirt.jpg",
-  },
-  {
-    type: "pants",
-    name: "something pants",
-    price: 140.0,
-    description: "pencil skirt",
-    color: "orange",
-    quantity: 999,
-    ImageURL:
-      "https://img.ssensemedia.com/images/f_auto,q_auto:best/202387F090217_1/kenzo-orange-straight-miniskirt.jpg",
-  },
-  {
-    type: "pants",
-    name: "something pants",
-    price: 130.0,
-    description: "tapered pants",
-    color: "white",
-    quantity: 999,
-    imageURL:
-      "https://img.ssensemedia.com/images/f_auto,q_auto:best/212771F087003_1/toteme-white-city-sport-trousers.jpg",
-  },
-  {
-    type: "pants",
-    name: "something pants",
-    price: 140.0,
-    description: "baggy pants",
-    color: "black",
-    quantity: 999,
-    imageURL:
-      "https://img.ssensemedia.com/images/b_white,g_center,f_auto,q_auto:best/212704M191021_1/wooyoungmi-black-baggy-carpenter-trousers.jpg",
-  },
-  {
-    type: "pants",
-    name: "something pants",
-    price: 85.0,
-    description: "long short",
-    color: "pink",
-    quantity: 999,
-    imageURL:
-      "https://img.ssensemedia.com/images/b_white,g_center,f_auto,q_auto:best/211236F088057_1/collina-strada-ssense-exclusive-pink-flower-patch-shorts.jpg",
-  },
-];
+// const customer = [
+//   {
+//     username: "Ataa",
+//     password: "password",
+//   },
+//   {
+//     username: "Lucy",
+//     password: "password1",
+//   },
+// ];
 
-const customer = [
-  {
-    username: "Ataa",
-    password: "password",
-  },
-  {
-    username: "Lucy",
-    password: "password1",
-  },
-];
-
-const admin = [
-  {
-    username: "admin1",
-    password: "adminone",
-    isAdmin: true,
-  },
-];
-// }
+// const admin = [
+//   {
+//     username: "admin1",
+//     password: "adminone",
+//     isAdmin: true,
+//   },
+// ];
+// // }
 
 // Creating Users
-const seed = async () => {
-  try {
-    await db.sync({ force: true });
-    const Clothes = await Promise.all(
-      shoes.map((shoe) => {
-        Clothing.create(shoe);
-      })
-    );
-    // await Clothing.bulkCreate(pants);
-    // await Clothing.bulkCreate(accessories);
-    // await Clothing.bulkCreate(shoes);
-    // await User.bulkCreate(customer);
-    // await User.create(admin);
-  } catch (err) {
-    console.log(red(err));
-  }
-  console.log(`seeded successfully`);
-};
+// const seed = async () => {
+//   try {
+//     await db.sync({ force: true });
+//     const Clothes = await Promise.all(
+//       shoes.map((shoe) => {
+//         Clothing.create(shoe);
+//       })
+//     );
+//     // await Clothing.bulkCreate(pants);
+//     // await Clothing.bulkCreate(accessories);
+//     // await Clothing.bulkCreate(shoes);
+//     // await User.bulkCreate(customer);
+//     // await User.create(admin);
+//   } catch (err) {
+//     console.log(red(err));
+//   }
+//   console.log(`seeded successfully`);
+// };
 
 // const seed = async () => {
 //   try {
@@ -294,18 +272,18 @@ const seed = async () => {
 //   }
 // };
 
-if (require.main === module) {
-  seed()
-    .then(() => {
-      console.log("Seeding success!");
-      db.close();
-    })
-    .catch((err) => {
-      console.error("Oh noes! Something went wrong!");
-      console.error(err);
-      db.close();
-    });
-}
+// if (require.main === module) {
+//   seed()
+//     .then(() => {
+//       console.log("Seeding success!");
+//       db.close();
+//     })
+//     .catch((err) => {
+//       console.error("Oh noes! Something went wrong!");
+//       console.error(err);
+//       db.close();
+//     });
+// }
 
 /*
  We've separated the `seed` function from the `runSeed` function.
@@ -336,4 +314,4 @@ if (module === require.main) {
 }
 
 // we export the seed function for testing purposes (see `./seed.spec.js`)
-module.exports = runSeed();
+module.exports = seed;
