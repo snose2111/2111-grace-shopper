ebf44fca0dcfc05611c95f384bbb67e01202cc89
Lucy Blagg <lvblagg@gmail.com>
2022-01-13 15:15:04 -0500

    all clothing component dynamically rendering via redux store

--- client/components/AllClothing.js
+++ client/components/AllClothing.js
@@ -1,67 +1,18 @@
 import React from "react";
 import { connect } from "react-redux";
-
-const dummyClothing = [
-  {
-    type: "shoes",
-    name: "Converse X COMME des GARÇONS PLAY",
-    price: 150.0,
-    description: "Unisex high top shoe",
-    quantity: 80,
-    color: "Grey",
-    rating: 4.66,
-    imageUrl:
-      "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_1086,w_724/c_scale,h_480/f_auto,q_auto/221246M236000_1/comme-des-garcons-play-grey-converse-edition-half-heart-chuck-70-sneakers.jpg",
-  },
-  {
-    type: "shoes",
-    name: "Converse X COMME des GARÇONS PLAY",
-    price: 150.0,
-    description: "Unisex low top shoe",
-    quantity: 68,
-    color: "Navy",
-    rating: 3.47,
-    imageUrl:
-      "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_1086,w_724/c_scale,h_480/f_auto,q_auto/221246M237001_1/comme-des-garcons-play-blue-converse-edition-half-heart-chuck-70-sneakers.jpg",
-  },
-  {
-    type: "shoes",
-    name: "Converse X COMME des GARÇONS PLAY",
-    price: 150.0,
-    description: "Unisex high top shoe",
-    quantity: 75,
-    color: "Navy",
-    rating: 4.32,
-    imageURL:
-      "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_1086,w_724/c_scale,h_480/f_auto,q_auto/221246M236001_1/comme-des-garcons-play-blue-converse-edition-half-heart-chuck-70-sneakers.jpg",
-  },
-  {
-    type: "shoes",
-    name: "Black Chuck Taylor All Star Platform Hi Sneakers",
-    price: 150.0,
-    description: "Unisex low top shoe",
-    quantity: 23,
-    color: "Grey",
-    rating: 4.89,
-    imageURL:
-      "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_1086,w_724/c_scale,h_480/f_auto,q_auto/221246M236000_1/comme-des-garcons-play-grey-converse-edition-half-heart-chuck-70-sneakers.jpg",
-  },
-  {
-    type: "shoes",
-    name: "Beige Summer Daze Chuck 70 sneakers",
-    price: 45.0,
-    description: "Unisex low top shoe",
-    quantity: 23,
-    color: "Grey",
-    rating: 2.39,
-    imageURL:
-      "https://img.ssensemedia.com/images/b_white,c_lpad,g_south,h_1086,w_724/c_scale,h_480/f_auto,q_auto/221246M236000_1/comme-des-garcons-play-grey-converse-edition-half-heart-chuck-70-sneakers.jpg",
-  },
-];
+import { fetchClothing } from "../store/clothing";
 
 export class AllClothing extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.props.getClothing();
+  }
+
   render() {
-    let clothing = dummyClothing;
+    let clothing = this.props.clothing;
     return (
       <div className="all-view">
         <div className="all-view-header">
@@ -76,7 +27,7 @@ export class AllClothing extends React.Component {
           {clothing.length ? (
             clothing.map((item) => {
               return (
-                <div className="all-view-single-card">
+                <div key={item.id} className="all-view-single-card">
                   <div className="all-view-img">
                     <img
                       src="https://img.ssensemedia.com/images/b_white,g_center,f_auto,q_auto:best/221288F120000_1/proenza-schouler-off-white-canvas-city-lug-lace-up-derbys.jpg"
@@ -101,7 +52,7 @@ export class AllClothing extends React.Component {
               );
             })
           ) : (
-            <p>No Clothing</p>
+            <p>Loading clothing...</p>
           )}
         </div>
       </div>
@@ -109,16 +60,14 @@ export class AllClothing extends React.Component {
   }
 }
 
-// const mapState = (state) => {
-//   return { clothing: state.allClothing };
-// };
+const mapState = (state) => {
+  return { clothing: state.clothing };
+};
 
-// const mapDispatch = (dispatch) => {
-//   return {
-//     getClothing: () => dispatch(fetchClothing()),
-//   };
-// };
+const mapDispatch = (dispatch) => {
+  return {
+    getClothing: () => dispatch(fetchClothing()),
+  };
+};
 
-// export default connect(mapState, mapDispatch)(AllClothing);
-
-export default AllClothing;
+export default connect(mapState, mapDispatch)(AllClothing);
