56879031930e5cb05152414ffec822b61bbb9954
aamponsah97 <ataaamponsah97@gmail.com>
2022-01-13 15:37:07 -0500

    special collection image and special button are now directly next to each other.

--- client/components/Homepage.js
+++ client/components/Homepage.js
@@ -28,25 +28,32 @@ export class Homepage extends React.Component {
       <div id="homepage">
         <h3>Hello!</h3>
         <div className="special-collection">
-          <div className="special-img">
+          <div id="special-img">
             <img src="https://skillz4kidzmartialarts.com/wp-content/uploads/2017/04/default-image.jpg" />
           </div>
-          <h4>Special Collection</h4>
-          <button>Shop Now</button>
+          <div id="special-info">
+            <h2>Special Collection</h2>
+            <button id="special-button">Shop Now</button>
+          </div>
         </div>
 
         <div className="categories">
+          <h2>Shop By Category</h2>
           <div className="category">
-            <h3>Tops</h3>
+            <img src="https://cdn.shopify.com/s/files/1/0047/0705/7752/products/PS-BellaCanvas-3001T-B2copy_0aa70df6-7c89-4b74-86ef-f64833bbef7e_256x.jpg?v=1639001512"/>
+            <h4>Tops</h4>
           </div>
           <div className="category">
-            <h3>Bottoms</h3>
+            <img src="https://cdn.shopify.com/s/files/1/0047/0705/7752/products/PS-BellaCanvas-3001T-B2copy_0aa70df6-7c89-4b74-86ef-f64833bbef7e_256x.jpg?v=1639001512"/>
+            <h4>Bottoms</h4>
           </div>
           <div className="category">
-            <h3>Shoes</h3>
+            <img src="https://cdn.shopify.com/s/files/1/0047/0705/7752/products/PS-BellaCanvas-3001T-B2copy_0aa70df6-7c89-4b74-86ef-f64833bbef7e_256x.jpg?v=1639001512"/>
+            <h4>Shoes</h4>
           </div>
           <div className="category">
-            <h3>Accessories</h3>
+            <img src="https://cdn.shopify.com/s/files/1/0047/0705/7752/products/PS-BellaCanvas-3001T-B2copy_0aa70df6-7c89-4b74-86ef-f64833bbef7e_256x.jpg?v=1639001512"/>
+            <h4>Accessories</h4>
           </div>
         </div>
 
