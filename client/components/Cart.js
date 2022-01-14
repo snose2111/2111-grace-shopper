8026bba551cfddfd583302304944a6dc4bea627e
Lucy Blagg <lvblagg@gmail.com>
2022-01-13 21:59:55 -0500

    added cart view and styling with dummy data

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -3,19 +3,19 @@ import React from "react";
 let dummyData = [
   {
     type: "accessories",
-    name: "Chubby Huggy Hoop, Silver (Single)",
+    name: "Chubby Huggy Hoop",
     price: 38.0,
-    description: "earring",
+    description: "Silver (Single) Earring",
     color: "silver",
     quantity: 24,
-    imageUrl:
+    ImageURL:
       "https://www.catbirdnyc.com/media/catalog/product/cache/76a94e0cda397936c0138d2cf05d7fe1/h/u/huggiehoop-ss-p1.jpg",
   },
   {
     type: "tops",
     name: "something top",
     price: 150.0,
-    description: "shirt",
+    description: "Sparkly Shirt",
     color: "green",
     quantity: 89,
     ImageURL:
@@ -23,9 +23,9 @@ let dummyData = [
   },
   {
     type: "bottoms",
-    name: "something pants",
+    name: "Something Pants",
     price: 140.0,
-    description: "pencil skirt",
+    description: "Pencil Skirt",
     color: "orange",
     quantity: 999,
     ImageURL:
@@ -33,24 +33,67 @@ let dummyData = [
   },
 ];
 
+const reducer = (previousValue, currentValue) => previousValue + currentValue;
+
 export class Cart extends React.Component {
   render() {
+    const subtotal = dummyData.map((item) => item.price).reduce(reducer);
+    const tax = subtotal * 0.08875;
     return (
       <div className="cart-view">
-        <div id="header">Shopping Bag</div>
         <div id="main">
           <div id="left">
+            <div id="header">Shopping Bag</div>
             {dummyData.map((item) => (
               <div key={item.id} id="cart-item">
-                <img src={item.ImageURL} width="150px" />
-                <div id="inner-cart-item">
-                  <span>{item.name}</span>
-                  <span>{item.description}</span>
+                <div id="left">
+                  <img id="cart-img" src={item.ImageURL} />
+                  <div id="inner-cart-item">
+                    <span id="item-name">{item.name}</span>
+                    <span>{item.description}</span>
+                    <div className="item-qty">
+                      <label htmlFor="quantity">Quantity: </label>
+                      <input
+                        name="quantity"
+                        type="number"
+                        min="0"
+                        max={item.quantity}
+                        defaultValue="1"
+                      />
+                      <button id="update-qty">Update</button>
+                    </div>
+                  </div>
+                </div>
+                <div id="right">
+                  <span>${item.price} USD</span>
+                  <button id="remove-button">Remove</button>
                 </div>
               </div>
             ))}
+            <div className="footer">
+              <div id="left">
+                <span>Total</span>
+                <span>Shipping Estimate</span>
+                <span>Tax</span>
+                <span className="total">Order Total</span>
+              </div>
+              <div id="right">
+                <span>${subtotal}</span>
+                <span>Calculated at checkout</span>
+                <span>${tax}</span>
+                <span className="total">${subtotal + tax}</span>
+              </div>
+            </div>
           </div>
-          <div id="right"></div>
+          <div id="right">
+            <div id="header">Checkout</div>
+            <div id="content">
+              <span>Continue to checkout.</span>
+              <div id="checkout-button">
+                <button id="checkout">Proceed to Checkout</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
