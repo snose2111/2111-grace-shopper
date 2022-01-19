9658b66a230d92bb2bbed452929389fe5f7f6364
Tenzin Choetso <tenzinchoetso93@gmail.com>
2022-01-19 10:53:26 -0500

    local storage half done

--- client/components/AllClothing.js
+++ client/components/AllClothing.js
@@ -11,6 +11,7 @@ export class AllClothing extends React.Component {
     this.state = {
       clothing: [],
     };
+    this.addNewItem = this.bind(this.addNewItem);
   }
 
   componentDidMount() {
@@ -33,22 +34,47 @@ export class AllClothing extends React.Component {
   }
 
   handleClick(evt) {
-    // this.props.addToCart(evt.target.value);
-    const itemId = evt.target.value;
-    console.log("ITEM ID", itemId);
-    let localCart = JSON.parse(window.localStorage.getItem("cart"));
-    if (!localCart) {
-      const currentItem = this.props.clothing.filter((item) => {
-        console.log("INSIDE FILTER", item.id);
-        return parseInt(itemId) === parseInt(item.id);
-      });
-      window.localStorage.setItem("cart", JSON.stringify(currentItem));
+    this.props.addToCart(evt.target.value);
+    // const itemId = evt.target.value;
+    // console.log("ITEM ID", itemId);
+    // let localCart = JSON.parse(window.localStorage.getItem("cart"));
+    // if (!localCart) {
+    //   const currentItem = this.props.clothing.filter((item) => {
+    //     console.log("INSIDE FILTER", item.id);
+    //     return parseInt(itemId) === parseInt(item.id);
+    //   });
+    //   window.localStorage.setItem("cart", JSON.stringify(currentItem));
+    // } else {
+    //   const currentItem = this.props.clothing.filter((item) => {
+    //     return parseInt(itemId) === parseInt(item.id);
+    //   });
+    //   localCart.push(JSON.stringify(currentItem));
+    //   window.localStorage.setItem("cart", JSON.stringify(localCart));
+    // }
+  }
+
+  addNewItem(clothingId, type, name, ImageUrl, price) {
+    let token = localStorage.getItem("token");
+    let tokened = token;
+    if (tokened) {
+      console.log("Hello token is here");
+      createOrderThunk(clothingId, price);
     } else {
-      const currentItem = this.props.clothing.filter((item) => {
-        return parseInt(itemId) === parseInt(item.id);
-      });
-      localCart.push(JSON.stringify(currentItem));
-      window.localStorage.setItem("cart", JSON.stringify(localCart));
+      var items = JSON.parse(localStorage.getItem("cart")) || [];
+      var item = items.find((item) => item.name === name);
+      if (item) {
+        item.count = Number(item.count) + 1;
+      } else {
+        items.push({
+          clothingId,
+          type,
+          name,
+          ImageUrl,
+          count: 1,
+          price,
+        });
+      }
+      localStorage.setItem("cart", JSON.stringify(items));
     }
   }
 
@@ -92,7 +118,12 @@ export class AllClothing extends React.Component {
                     <button
                       id="all-view-item-button"
                       value={item.id} // want to pass in item Id probably
-                      onClick={this.handleClick}
+                      onClick={this.handleClick(
+                        item.name,
+                        item.price,
+                        item.imageUrl,
+                        item.type
+                      )}
                     >
                       Add to Cart
                     </button>
